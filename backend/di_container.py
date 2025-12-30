aeab3b0991ad423c14627e2c7c78e2bcae67e503
myagkov <you@example.com>
2025-12-30 17:37:03 +0300

    first_decision

--- backend/di_container.py
+++ backend/di_container.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from dependency_injector import containers, providers
 from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession, create_async_engine
 
@@ -5,24 +7,25 @@ from backend.db_connection import ADB_URL, SDB_URL
 from backend.src.modules.shared.unit_of_work import UnitOfWork
 
 
+@providers.Factory
+@asynccontextmanager
+async def script_session():
+    session_maker = container.script_sessionmaker()
+    async with session_maker() as session:
+        yield session
+        
+@providers.Factory
+@asynccontextmanager
+async def admin_session():
+    session_maker = container.admin_sessionmaker()
+    async with session_maker() as session:
+        yield session
+
+
+
 class Container(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(modules=["backend.src.app.api.auth"])
 
-    # admin_sessionmaker = providers.Singleton(
-    #     async_sessionmaker, bind=ADB_ENGINE, class_=AsyncSession, expire_on_commit=False
-    # )
-    # script_sessionmaker = providers.Singleton(
-    #     async_sessionmaker, bind=SDB_ENGINE, class_=AsyncSession, expire_on_commit=False
-    # )
-
-    # admin_session = providers.Resource(
-    #     async_sessionmaker, bind=ADB_ENGINE, class_=AsyncSession, expire_on_commit=False
-    # )
-    # script_session = providers.Resource(
-    #     async_sessionmaker, bind=SDB_ENGINE, class_=AsyncSession, expire_on_commit=False
-    # )
-
-    # Фабрики для создания engine
     admin_engine = providers.Singleton(
         create_async_engine,
         url=ADB_URL,
@@ -49,10 +52,6 @@ class Container(containers.DeclarativeContainer):
         class_=AsyncSession,
         expire_on_commit=False
     )
-    
-    # Ресурсы для сессий
-    admin_session = providers.Resource(admin_sessionmaker)
-    script_session = providers.Resource(script_sessionmaker)
 
     admin_uow = providers.Factory(UnitOfWork, session=admin_session)
     script_uow = providers.Factory(UnitOfWork, session=script_session)
